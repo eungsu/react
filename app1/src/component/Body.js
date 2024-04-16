b94b13650f9b74bc3e4103197ad288e5bb307713
eungsu <eungsulee@hanmail.net>
2024-04-16 16:27:57 +0900

    [Feat] props로 값 전달하기

--- app1/src/component/Body.js
+++ app1/src/component/Body.js
@@ -22,6 +22,17 @@
 // export default Body;
 
 // 구조분해할당으로 여러 개의 값 사용하기
+// function Body({name, location, favorites}) {
+//     return (
+//         <div>
+//             <p>{name}님은 {location}에 살고 있습니다.</p>
+//             <p>{name}님은 {favorites.length}개의 과일을 좋아합니다.</p>
+//         </div>
+//     );
+// }
+// export default Body;
+
+// Props로 전달받은 값에 대해서 기본값을 미리 설정하기
 function Body({name, location, favorites}) {
     return (
         <div>
